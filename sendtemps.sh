cfd957d458e11b0e1514a09aeb1eea49742d3fdd
Jon Green <jon@jongreen.co.uk>
2018-02-04 20:45:19 +0000

    Add script to send temps

---
+++ sendtemps.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ "$1" == "raw" ]
+then
+	        mode=raw
+	fi
+
+	outside=28-0317318552ff
+	dining=28-0317322bcaff
+	heating=28-031731d6a6ff
+	lounge=28-041731d205ff
+	underfloor=28-031731e47dff
+
+
+	function getTemp {
+		rawtemp=`cat /sys/bus/w1/devices/$1/w1_slave | awk 'NR==2' | awk -F '=' '{print $2}'`
+		realtemp=`echo "scale=1; $rawtemp / 1000" | bc`
+
+		if [ "$mode" == "raw" ]
+		then
+			        echo $2: $rawtemp
+			else
+				echo $2: $realtemp
+			fi
+		}
+
+
+		getTemp $outside outside
+		getTemp $dining diningroom
+		getTemp $lounge lounge
+		getTemp $heating heating
+		getTemp $underfloor underfloor
