7ffdbb0b45d946585ce5862222ec8131ac3bb4a4
jon-green <jon@jongreen.co.uk>
2018-02-04 20:52:54 +0000

    Update updatetemps.sh

--- updatetemps.sh
+++ updatetemps.sh
@@ -18,14 +18,14 @@ heatingreal=`echo "scale=1; $heatingraw / 1000" | bc`
 #loungereal=`echo "scale=1; $loungeraw / 1000" | bc`
 loungereal=NaN
 
-underfloorraw=`cat /tmp/rawtemps | grep underfloor | awk {'print $2'}`
-underfloorreal=`echo "scale=1; $underfloorraw / 1000" | bc`
+bedroomraw=`cat /tmp/rawtemps | grep underfloor | awk {'print $2'}`
+bedroomreal=`echo "scale=1; $underfloorraw / 1000" | bc`
 #underfloorreal=NaN
 
 echo $outsidereal
 echo $diningreal
 echo $heatingreal
 echo $loungereal
-echo $underfloorreal
+echo $bedroomreal
 
-/usr/bin/rrdtool update /home/jon/temps/temps.rrd N:$outsidereal:$diningreal:$heatingreal:$loungereal:$underfloorreal
+/usr/bin/rrdtool update /home/jon/temps/temps.rrd N:$outsidereal:$diningreal:$heatingreal:$loungereal:$bedroomreal
